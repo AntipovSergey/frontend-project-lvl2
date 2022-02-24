41c1a22a746184be3eb0cea39546b87746f979e7
Antipov Sergey <antip99999@mail.ru>
2022-02-24 16:07:13 +0300

    Create fixtures dir and add there files.json. Create src folder and add there genDiff.js(implemented). Connect genDiff.js to gendiff.js (create .action in commander). Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-### Hexlet tests and linter status:
+### How to install the repository
+1. Clone the repository
+2. Run the command: make install
+3. Run the command: npm link
+
+### About the project
+
+Gendiff is a program that determines the difference between two data structures. 
+
+This is a popular task, for which there are many online services, for example http://www.jsondiff.com /. A similar mechanism is used when outputting tests or when automatically tracking changes in configuration files.
+
+Utility Features:
+
+1. Support for different input formats: yaml, json
+2. Generating a report in the form of:
+  1. Plain text
+  2. Stylish 
+  3. JSON
+
+Workflow badges
+1. Hexlet tests and linter status: <br>
 [![Actions Status](https://github.com/AntipovSergey/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/AntipovSergey/frontend-project-lvl2/actions)
+
+### using the program gendiff 
+[![asciicast](https://asciinema.org/a/fOHi0FIsS33wr9XjrelLNayW3.svg)](https://asciinema.org/a/fOHi0FIsS33wr9XjrelLNayW3)
---
+++ src/genDiff.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const readFile = (filename) => {
+  const file = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', filename), 'utf-8');
+  return JSON.parse(file);
+};
+
+const genDiff = (file1, file2) => {
+  const firstObject = readFile(file1);
+  const secondObject = readFile(file2);
+  const keysFirst = Object.keys(firstObject);
+  const keysSecond = Object.keys(secondObject);
+  const allKeys = _.union(keysFirst, keysSecond).sort();
+  const result = {};
+
+  for (const key of allKeys) {
+    if (_.has(firstObject, key) && _.has(secondObject, key)) {
+      if (firstObject[key] === secondObject[key]) {
+        result[`  ${[key]}`] = firstObject[key];
+      } else if (firstObject[key] !== secondObject[key]) {
+        result[`- ${[key]}`] = firstObject[key];
+        result[`+ ${[key]}`] = secondObject[key];
+      }
+    } else if (_.has(firstObject, key) && !_.has(secondObject, key)) {
+      result[`- ${[key]}`] = firstObject[key];
+    } else if (!_.has(firstObject, key) && _.has(secondObject, key)) {
+      result[`+ ${[key]}`] = secondObject[key];
+    }
+  }
+
+  console.log(`{\n${Object.entries(result).map((item) => `  ${item.join(': ')}`).join('\n')}\n}`);
+};
+
+export default genDiff;
